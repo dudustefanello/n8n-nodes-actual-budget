15191d8f1a82a4873e1fa76a0d0fe1d04e96c0f5
Eduardo Stefanello <eduardo@stefanello.cc>
2025-06-21 16:21:50 -0300

    publish

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,10 @@ function copyIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
 	const nodeDestination = path.resolve('dist', 'nodes');
 
-	src(nodeSource).pipe(dest(nodeDestination));
+	src(nodeSource).on('error', console.error).pipe(dest(nodeDestination));
 
 	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
 	const credDestination = path.resolve('dist', 'credentials');
 
-	return src(credSource).pipe(dest(credDestination));
+	return src(credSource).on('error', console.error).pipe(dest(credDestination));
 }
